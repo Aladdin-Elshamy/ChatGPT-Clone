85f567fef0c77af55c8d835cb971ff7038e34ccd
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2025-01-03 13:38:14 +0200

    Change api key

--- src/shared/Input.jsx
+++ src/shared/Input.jsx
@@ -7,7 +7,7 @@ export default function Input({ chat, setChat }) {
   const [loading, setLoading] = useState(false);
   const [rotate, setRotate] = useState(false);
   const genAI = new GoogleGenerativeAI(
-    "AIzaSyCboNGhaTPgPsjbUM26JHv9xawHsUkJ4Ec"
+    "AIzaSyAuPdoZHMUieDFn04qafijPhlPdeH5QtYI"
   );
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   async function handleSubmit(e) {
